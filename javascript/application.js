9ee9caff187f19110442411c553d1d4158b5dcdd
Valerii Zuev <valera.zuev.zva@gmail.com>
2018-05-25 11:41:50 +0300

    изменение check, move, добавление новых ф-й, незначительные исправления

--- javascript/application.js
+++ javascript/application.js
@@ -10,8 +10,26 @@ function init () {
 	console.log(A);
 	td11.innerHTML=0;
 }
-function move (dir, A){
-	console.log("func move (зависит от всех троих)");
-	var A1=rotate(A);
-	
+
+window.addEventListener("load",maiт,false);
+function main(){
+	//добавить вызов onrightpress и проч по нажатиям клавиш
 }
+
+function onrightpress () {
+	//вставить получение A из таблицы
+	var A=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+	/*var A=[];
+	for (var i=0; i < 4; i++){
+		for (var j=0; j<4; j++){
+			var current_td =null;
+			switch (10*i+j){
+				case
+			}
+		}
+	}*/
+	move (0,A); //ход направо
+}
+function onleftpress() {}
+function ondownress() {}
+function onuppress() {}
--- javascript/calc.js
+++ javascript/calc.js
@@ -1,3 +1,15 @@
+function move (dir, A){
+	console.log("func move (зависит от всех троих)");
+	var A1=rotate(dir, A);
+	var A2=calc_new_pos(A1);
+	var A3=rotate_back(A2);
+	draw(A3);
+	var flag=check(A3);
+	if(flag==false){
+		gameover();
+	}
+}
+
 function rotate(dir, A){
 	console.log("func rotate (ответственн Миша)");
 	//необходимо вывести матрицу, повёрнутую по часовой стрелке
@@ -11,7 +23,22 @@ function rotate_back(dir, A){
 	return A;
 }
 function check(A){
-	var T=A;
+	console.log("func check (ответственн Миша)");
+	var T=null;
+	var flag=false;
+	for (var i=0; i<4; i++){
+		T=calc_new_pos(A);
+		for (var j=0; j<4; j++){
+			for (var k=0; k<4; k++){
+				if(T[j][k]!=A[j][k]){
+					flag=true;
+					return flag;
+				}
+			}
+		}
+		A=rotate(1,A); //повернуть один раз
+	}
+	return flag; //false=>gameover
 }
 
 function calc_new_pos(A){
