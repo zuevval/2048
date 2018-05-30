cf40ebe3f0af534c6130d79c3f2cb04623300658
Valerii Zuev <valera.zuev.zva@gmail.com>
2018-05-30 09:29:29 +0300

    Исправлен check

--- javascript/calc.js
+++ javascript/calc.js
@@ -63,69 +63,78 @@ function check(A){
 	return flag; //false=>gameover
 }
 
-function calc_new_pos(A){
-	console.log("func calc_new_pos (ответственн Глеб)")
+function calc_new_pos(A) {
+    console.log("func calc_new_pos (ответственн Глеб)")
     //var Sum=0;
-	var check_var = 0
-    function shift(B){ //функция сдвига
-        for(var i=0;i<4;i++){
-            for(var j=3;j>0;j--)//двигает с конца строки
+    var check_var = 0
+
+    function shift(B) { //функция сдвига
+        for (var i = 0; i < 4; i++) {
+            for (var j = 3; j > 0; j--)//двигает с конца строки
             {
-                if(B[i][j-1]!=0&&B[i][j]==0){ //при нахождении нулевого элемента сдвигает в право и переходит снова в конец строки
-                    B[i][j]=B[i][j-1];
-                    B[i][j-1]=0;
-                    j=3;
+                if (B[i][j - 1] != 0 && B[i][j] == 0) { //при нахождении нулевого элемента сдвигает в право и переходит снова в конец строки
+                    B[i][j] = B[i][j - 1];
+                    B[i][j - 1] = 0;
+                    j = 3;
                 }
             }
         }
-		if (check_var != 1){
-			shiftAndClap(B)
-		}
+        if (check_var != 1) {
+            shiftAndClap(B)
+        }
         return B;
     }
-    function addRandNum(B){//функия добавления в рандомное пустое место 2 или 4 с шансом 75/25
-        var placeOfNullI=[];//два массива, запоминающие строку и столбик нулевых элементов
-        var placeOfNullJ=[];
-        var n=Math.floor(Math.random()*3+2);//генерация 2 или 4
-        if (n<4){n=2;}
-        for(var i=0;i<4;i++){
-            for(var j=0;j<4;j++){
-                if(B[i][j]==0){
+
+    function addRandNum(B) {//функия добавления в рандомное пустое место 2 или 4 с шансом 75/25
+        var placeOfNullI = [];//два массива, запоминающие строку и столбик нулевых элементов
+        var placeOfNullJ = [];
+        var n = Math.floor(Math.random() * 3 + 2);//генерация 2 или 4
+        if (n < 4) {
+            n = 2;
+        }
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 4; j++) {
+                if (B[i][j] == 0) {
                     placeOfNullI.push(i);//запоминание мест нулей
                     placeOfNullJ.push(j);
                 }
             }
         }
-	if (placeOfNullI.length<1){return B;}
-        var randPlace=Math.floor(Math.random()*(placeOfNullI.length));//рандомный выбор места из списка всех нулей
-        var i=placeOfNullI[randPlace];
-        var j=placeOfNullJ[randPlace];
-        B[i][j]=n;//присваивание элементу массива с рандомным номером строки и столбца 2 или 4 в зависимости от генерации
+        if (placeOfNullI.length < 1) {
+            return B;
+        }
+        var randPlace = Math.floor(Math.random() * (placeOfNullI.length));//рандомный выбор места из списка всех нулей
+        var i = placeOfNullI[randPlace];
+        var j = placeOfNullJ[randPlace];
+        B[i][j] = n;//присваивание элементу массива с рандомным номером строки и столбца 2 или 4 в зависимости от генерации
     }
-    function shiftAndClap(B){//функция сдвига и "схлопывания" чисел так же работает с конца
-        for(var i=3;i>-1;i--){
-            for(var j=3;j>0;j--){
-                if (B[i][j-1]==B[i][j]){//при нахождении одинаковых чисел сливает в сумму на месте правого числа
-                    B[i][j]*=2;
-                    B[i][j-1]=0;
-                    Sum+=B[i][j];//добавление в общий счет суммы "схлопнутых" элементов
+
+    function shiftAndClap(B) {//функция сдвига и "схлопывания" чисел так же работает с конца
+        for (var i = 3; i > -1; i--) {
+            for (var j = 3; j > 0; j--) {
+                if (B[i][j - 1] == B[i][j]) {//при нахождении одинаковых чисел сливает в сумму на месте правого числа
+                    B[i][j] *= 2;
+                    B[i][j - 1] = 0;
+                    Sum += B[i][j];//добавление в общий счет суммы "схлопнутых" элементов
 
                 }
             }
         }
-		check_var = 1
-        B=shift(B);//после этого сдвигает вправо
+        check_var = 1
+        B = shift(B);//после этого сдвигает вправо
         addRandNum(B);//добавление в рандомное место 2 или 4
-		check_var = 0
+        check_var = 0
         return B;
     }
-    var A1=[];
-    for (var i=0; i<4; i++){
-        A1[i]=[];
-        for (var j=0; j<4; j++){
-            A1[i][j]=A[i][j]; //защита от изменнения A
+
+    var A1 = [];
+    for (var i = 0; i < 4; i++) {
+        A1[i] = [];
+        for (var j = 0; j < 4; j++) {
+            A1[i][j] = A[i][j]; //защита от изменнения A
         }
     }
     //console.log(A1);
-    var A2=shift(A);
+    var A2 = shift(A1);
     return A2;
+}
